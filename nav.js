e976a68c17c350f00cbc4f065ac572212639fe4b
vabhi2024 <vabhi2024@gmail.com>
2025-11-15 15:06:44 +0530

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-
-function openMenu() {
-    document.getElementById("full-screen-menu").classList.add("active");
-}
-
-function closeMenu() {
-    document.getElementById("full-screen-menu").classList.remove("active");
-}
